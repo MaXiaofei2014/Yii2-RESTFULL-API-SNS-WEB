96bd1acb9819d9ba3ebba6da51e7b542cf8542ed
fengahan <544976880@qq.com>
2017-07-19 23:11:19 +0800

    add click users comments list

--- api/modules/v1/controllers/UserTalkController.php
+++ api/modules/v1/controllers/UserTalkController.php
@@ -2,7 +2,10 @@
 
 namespace api\modules\v1\controllers;
 use api\models\UserTalk;
+use common\models\TalkComment;
 use yii\data\ActiveDataProvider;
+use yii\db\Query;
+
 use yii\rest\ActiveController;
 
 
@@ -17,11 +20,14 @@ class UserTalkController extends ActiveController
         unset($actions['index'], $actions['create'], $actions['view']);
         return $actions;
     }
-
+    /**
+     *用户说说列表
+     *
+     **/
     public function actionIndex()
     {
-
-        $query=UserTalk::find();
+        $user_id=(int)\Yii::$app->request->get('user_id');
+        $query=UserTalk::find()->where(['user_id'=>$user_id]);
         $provider = new ActiveDataProvider([
             'query' =>$query,
             'pagination' => [
@@ -37,12 +43,17 @@ class UserTalkController extends ActiveController
       return $provider;
     }
 
+    /**
+     * @return static
+     * 说说详情
+     */
     public function actionView()
     {
-        $param=\Yii::$app->request->get();
-        $info=UserTalk::findOne($param['id']);
+          $param=\Yii::$app->request->get();
+          $info=UserTalk::findOne($param['id']);
+          return $info;
 
-        return $info;
+
     }
 }
 
